c2f4930a96d71be5be55b58369ecc42b1b99a584
kelliot1 <kelliot1@ad3.ucdavis.edu@pc54.cs.ucdavis.edu>
2019-11-05 17:32:40 -0800

    Finish phase 1

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -62,15 +62,18 @@ int sem_up(sem_t sem)
 {
 	pthread_t tid;
 
+	/* Check if sem exists */
+	if (sem == NULL)
+		return -1;
+
 	enter_critical_section();
 
-	if (queue_length(sem->blockedQueue) == 0)
+	if (queue_length(sem->blockedQueue) != 0)
 	{
-
-		thread_block();
+		queue_dequeue(sem->blockedQueue, (void**)&tid);
+		thread_unblock(tid);
 	}
 
-	//queue_dequeue(sem->blockedQueue);
 	sem->count++;
 
 	exit_critical_section();
@@ -80,6 +83,24 @@ int sem_up(sem_t sem)
 
 int sem_getvalue(sem_t sem, int *sval)
 {
+	int numInQueue = 0;
+
+	if (sem == NULL || sval == NULL)
+		return -1;
+
+	enter_critical_section();
+
+	if (sem->count > 0)
+		*sval = sem->count;
+	else if (sem->count == 0)
+	{
+		numInQueue = queue_length(sem->blockedQueue);
+		*sval = -numInQueue;
+	}
+
+	exit_critical_section();
+
+	return 0;
 	/* TODO Phase 1 */
 }
 
